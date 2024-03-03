06e46aee4d71ac279e84cca761b6ee85172afd81
mzulfucelik <muhammedzulfucelik@gmail.com>
2024-03-03 22:52:23 +0300

    SignUp Pages

---
+++ frontend/src/pages/SignUp/index.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+export function SignUp() {
+    const [username, setUsername] =useState()
+    const [email, setEmail] =useState()
+    const [password, setPassword] =useState()
+    const [passwordRepeat, setPasswordRepeat] =useState()
+
+    console.log(username)
+  return (
+    <>
+      <h1>SignUp</h1>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input id="username" onChange={(event) => setUsername(event.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="email">E-mail</label>
+        <input id="email" onChange={(event) => setEmail(event.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" onChange={(event) => setPassword(event.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="passwordRepeat">Password Repeat</label>
+        <input id="passwordRepeat" type="password" onChange={(event) => setPasswordRepeat(event.target.value)} />
+      </div>
+      <button disabled={!password || password !== passwordRepeat} >Sign Up</button>
+    </>
+  );
+}
