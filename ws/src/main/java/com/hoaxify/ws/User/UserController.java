6b87ab963923c991b83fe5ca04fc16c036f4cc99
mzulfucelik <muhammedzulfucelik@gmail.com>
2024-03-04 22:57:42 +0300

    Create User Cont

--- ws/src/main/java/com/hoaxify/ws/User/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/User/UserController.java
@@ -1,2 +1,15 @@
-package com.hoaxify.ws.User;public class UserController {
+package com.hoaxify.ws.User;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @CrossOrigin
+    @PostMapping("api/v1/user")
+    void createUser(){
+
+    }
 }
